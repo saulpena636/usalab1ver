060c06f392484f227d45ba92a8d2fb083b478f3d
saulpena636 <suoerpenniya@gmail.com>
2024-09-09 11:59:38 -0600

    Layout corregido

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -158,6 +158,21 @@
         </div>
       </div>
 
+      <div class="separador2"></div>
+      <div class="equipoCont">
+        <div class="equipotitle">Equipo a cargo</div>
+        <div class="eq">
+          <div class="eq1">
+            <img src="{{ asset('photos/image1.png') }}" alt="" srcset="">
+            <div class="nome1">Ing. Carlos Alberto Martinez Sandoval</div>
+          </div>
+          <div class="eq1">
+            <img src="{{ asset('photos/image2.png') }}" alt="" srcset="">
+            <div class="nome2">Ing. David del Castillo Lale</div>
+          </div>
+        </div>
+      </div>
+
       <div class="separador4"></div>
       <div class="hablanosContainer">
         <div class="hablanos">
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
 <head>
   <meta charset="UTF-8">
@@ -7,6 +7,7 @@
   <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <link href="{{ asset('css/main.css') }}" type="text/css" rel="stylesheet">
   @yield('cssp')
   <title>@yield('title')</title>
 </head>
@@ -24,21 +25,98 @@
         </div>
         <div class="menu1">
           <div class="acercade">
-            <div class="acercadetxt">Acerca de</div>
+            <div class="navbartxt">Acerca de</div>
+            <div class="acercadeCont">
+              <div class="adcontent">
+                <div class="navbartxt">Mision y vision</div>
+              </div>
+              <div class="adcontent">
+                <div class="navbartxt">Instalaciones</div>
+              </div>
+              <div class="adcontent">
+                <div class="navbartxt">Equipo a cargo</div>
+              </div>
+              <div class="adcontent">
+                <div class="navbartxt">Trayectoria</div>
+              </div>
+              <div class="adcontent">
+                <div class="navbartxt">colaboracion</div>
+              </div>
+              <div class="adcontent">
+                <div class="navbartxt">Logros</div>
+              </div>
+              <div class="adcontent">
+                <div class="navbartxt">Testimonios</div>
+              </div>
+            </div>
           </div>
           <div class="servicios">
-            <div class="serviciostxt">Servicios</div>
+            <div class="navbartxt">Servicios</div>
             <div class="serviciosCont">
-              <div class="drcontent">
-                <div class="comercialtxt">Comercial</div>
+              <div class="srcontent">
+                <div class="navbartxt">Industria</div>
               </div>
-              <div class="drcontent">
-                <div class="academicotxt">Academico</div>
+              <div class="srcontent">
+                <div class="navbartxt">Academia</div>
+              </div>
+            </div>
+          </div>
+          <div class="investigaciones">
+            <div class="navbartxt">Investigaciones</div>
+            <div class="investigacionesCont">
+              <div class="incontent">
+                <div class="navbartxt">Proyectos Actuales</div>
+              </div>
+              <div class="incontent">
+                <div class="navbartxt">Publicaciones</div>
+              </div>
+              <div class="incontent">
+                <div class="navbartxt">Líneas de investigación</div>
+              </div>
+              <div class="incontent">
+                <div class="navbartxt">Mas destacados</div>
+              </div>
+            </div>
+          </div>
+          <div class="formacion">
+            <div class="navbartxt">Formacion Académica</div>
+            <div class="formacionCont">
+              <div class="facontent">
+                <div class="navbartxt">Pruebas de Usabilidad</div>
+              </div>
+              <div class="facontent">
+                <div class="navbartxt">Practicas Profesionales</div>
+              </div>
+              <div class="facontent">
+                <div class="navbartxt">Servicio Social</div>
+              </div>
+              <div class="facontent">
+                <div class="navbartxt">Tesis</div>
+              </div>
+              <div class="facontent">
+                <div class="navbartxt">Maestría</div>
               </div>
             </div>
           </div>
           <div class="eventos">
-            <div class="eventostxt">Eventos</div>
+            <div class="navbartxt">Eventos</div>
+            <div class="eventosCont">
+              <div class="evcontent">
+                <div class="navbartxt">Últimos Eventos</div>
+              </div>
+              <div class="evcontent">
+                <div class="navbartxt">Conferencias</div>
+              </div>
+              <div class="evcontent">
+                <div class="navbartxt">Talleres</div>
+              </div>
+              <div class="evcontent">
+                <div class="navbartxt">Cursos</div>
+              </div>
+              <div class="evcontent">
+                <div class="navbartxt">Concursos</div>
+              </div>
+            </div>
           </div>
           <div class="idioma">
             <img src="{{ asset('icons/fontisto_world-o.svg') }}" alt="">
@@ -53,7 +131,13 @@
     </main>
 
     <footer>
-      
+      <img class="utmfooter" src="{{ asset('img/Logo-UTM-1.png') }}" />
+      <img class="usalabfooter" src="{{ asset('img/logo Usalab 3-03 1.png') }}" />
+      <div class="footertxt">+52 953 532 0399<br/><br/>Av. Doctor Modesto Seara Vázquez #1, Acatlima, 69000 Heroica Cdad. de Huajuapan de León, Oax. Mexico<br/><br/></div>
+      <div class="logoslink">
+        <img class="facebook" src="{{ asset('icons/mdi_facebook.svg') }}" />
+        <img class="link12" src="{{ asset('icons/ic_round-link.svg') }}" />
+      </div>
     </footer>
 
   </div>
