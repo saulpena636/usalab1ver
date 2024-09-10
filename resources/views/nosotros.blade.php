300ee733817212d4680d67835ebd9c0f9a8fc6b5
saulpena636 <suoerpenniya@gmail.com>
2024-09-10 11:18:30 -0600

    nosotros 1ver

--- resources/views/nosotros.blade.php
+++ resources/views/nosotros.blade.php
@@ -75,5 +75,139 @@
     <div class="trayectoriaContent">
         
     </div>
+    <div class="separador2"></div>
+    <div class="colaboracionesContent">
+        <div class="colaboracionestitle">Colaboraciones</div>
+    </div>
+    <div class="separador2"></div>
+    <div class="logrosContent">
+        <div class="logrostitle">Logros en participacion de concurso</div>
+        <div class="separador5"></div>
+        <div class="logroscont">
+            <div class="cuadroverde">
+                <div class="cuadrotitle">ACM SIGCHI 2007</div>
+                <div class="cuadrop">Segundo Lugar San josé, California</div>
+            </div>
+            <div class="cuadroverde">
+                <div class="cuadrotitle">ACM SIGCHI 2008</div>
+                <div class="cuadrop">Primer Lugar, Florencia Italia</div>
+            </div>
+            <div class="cuadroazul">
+                <div class="cuadrotitle">MexIHC 2010</div>
+                <div class="cuadrop">Primer Lugar, San Luis Potosí</div>
+            </div>
+            <img class="cuadroutm" src="{{ asset('img/Logo UTM Color 1.png')}}" alt="" srcset="">
+
+            <div class="cuadrogris">
+                <div class="cuadrotitle">iSDC UPA 2011</div>
+                <div class="cuadrop">Primer Lugar, Atlante, Georgia</div>
+            </div>
+            <div class="cuadroverde">
+                <div class="cuadrotitle">ACM SIGCHI 2011</div>
+                <div class="cuadrop">Finalistas, Vancouver, Canadá</div>
+            </div>
+            <div class="cuadroceleste">
+                <div class="cuadrotitle">CLIHC 2011</div>
+                <div class="cuadrop">Primer y Sgundo Lugar, Porto do Galinhas, Brasil</div>
+            </div>
+            <div class="cuadroazul">
+                <div class="cuadrotitle">MexIHC 2012</div>
+                <div class="cuadrop">Primer y Tercer Lugar, ITAM Mexico</div>
+            </div>
+
+            <div class="cuadroverde">
+                <div class="cuadrotitle">ACM SIGHI 2013</div>
+                <div class="cuadrop">Segundo Lugar, Finalistas, Paris, Francia</div>
+            </div>
+            <div class="cuadroceleste">
+                <div class="cuadrotitle">CLIHC 2013</div>
+                <div class="cuadrop">Primer lugar y Tercer Lugar, ITAM Mexico</div>
+            </div>
+            <div class="cuadroazul">
+                <div class="cuadrotitle">MexIHC 2014 ENC 2014</div>
+                <div class="cuadrop">Primer y Tercer Lugar, Nova Universitas, Oaxaca</div>
+            </div>
+            <div class="cuadroceleste">
+                <div class="cuadrotitle">CLIHC 2015</div>
+                <div class="cuadrop">Primer, Segundo y Cuarto Lugar, Cordoba, Argentina</div>
+            </div>
+
+            <div class="cuadroazul">
+                <div class="cuadrotitle">MexIHC 2016</div>
+                <div class="cuadrop">Tercer y Cuarto lugar, Colima, Colima</div>
+            </div>
+            <div class="cuadroverde">
+                <div class="cuadrotitle">ACM SIGCHI 2017</div>
+                <div class="cuadrop">Finalistas, Denver Colorado</div>
+            </div>
+            <div class="cuadroceleste">
+                <div class="cuadrotitle">CLIHC 2017</div>
+                <div class="cuadrop">Primer y segundo lugar, Antigua Guatemala</div>
+            </div>
+            <div class="cuadroazul">
+                <div class="cuadrotitle">MexIHC 2018</div>
+                <div class="cuadrop">Segundo y Tercer Lugar, Mérida Yucatan</div>
+            </div>
+
+            <img class="cuadrousalab" src="{{ asset('img/logo Usalab 3-03 1.png') }}" alt="" srcset="">
+            <div class="cuadroceleste">
+                <div class="cuadrotitle">CLIHC 2019</div>
+                <div class="cuadrop">Segundo Lugar, Panama</div>
+            </div>
+            <div class="cuadroverde">
+                <div class="cuadrotitle">ACM SIGCHI 2023</div>
+                <div class="cuadrop">Segundo Lugar, Hamburgo, Alemania</div>
+            </div>
+            <div class="cuadroceleste">
+                <div class="cuadrotitle">CLIHC 2023</div>
+                <div class="cuadrop">Primer lugar, Puebla, México</div>
+            </div>
+        </div>
+    </div>
+
+    <div class="separador2"></div>
+    <div class="testimoniosContent">
+        <div class="testimoniostitle">Algunos Testimonios</div>
+        <div class="testimonioscont">
+            <div class="testimoniocard">
+                <img class="testimoniosimg" src="{{ asset('photos/testimonio1.png') }}" alt="" srcset="">
+                <div class="testimoniotitulo">Ana Martínez, Directora de Producto, Tech Innovations</div>
+                <div class="testimoniop">"El equipo de UsaLab transformó nuestra interfaz de usuario, mejorando significativamente la experiencia del cliente. Las recomendaciones prácticas y el análisis detallado nos ayudaron a identificar puntos críticos y a implementar mejoras efectivas."</div>
+            </div>
+
+            <div class="testimoniocard">
+                <img class="testimoniosimg" src="{{ asset('photos/testimonio2.png') }}" alt="" srcset="">
+                <div class="testimoniotitulo">Laura Sánchez, Gerente de Proyectos, WebDesign Co.</div>
+                <div class="testimoniop">"Gracias a UsaLab, logramos optimizar nuestro sitio web y aumentar la satisfacción del usuario. Su enfoque profesional y su meticulosa atención al detalle marcaron una gran diferencia en nuestro proyecto."</div>
+            </div>
+
+            <div class="testimoniocard">
+                <img class="testimoniosimg" src="{{ asset('photos/testimonio3.png') }}" alt="" srcset="">
+                <div class="testimoniotitulo">Miguel Fernández, Fundador, StartupX</div>
+                <div class="testimoniop">"El análisis de usabilidad realizado por UsaLab fue crucial para el lanzamiento exitoso de nuestra aplicación. Su comprensión de las necesidades del usuario y las soluciones propuestas fueron clave para nuestro éxito."</div>
+            </div>
+            <div class="testimoniocard">
+                <img class="testimoniosimg" src="{{ asset('photos/testimonio4.png') }}" alt="" srcset="">
+                <div class="testimoniotitulo">Carlos López, Jefe de Desarrollo, Soft Solutions</div>
+                <div class="testimoniop">"Trabajar con UsaLab fue una experiencia increíble. No solo recibimos un informe detallado de usabilidad, sino que también nos brindaron sesiones de asesoramiento para entender mejor a nuestros usuarios. ¡Altamente recomendados!"</div>
+            </div>
+            <div class="testimoniocard">
+                <img class="testimoniosimg" src="{{ asset('photos/testimonio5.png') }}" alt="" srcset="">
+                <div class="testimoniotitulo">María González, CEO, Innovatech</div>
+                <div class="testimoniop">"El equipo de UsaLab nos ayudó a transformar nuestro sitio web, haciendo que la navegación y la interacción sean mucho más fluidas para nuestros usuarios. Su experiencia en usabilidad es evidente y los resultados hablan por sí mismos."</div>
+            </div>
+        </div>
+    </div>
+
+    <div class="separador2"></div>
+    <div class="hablanosContainer">
+        <div class="hablanos">
+            <div class="hablanos-title">Háblanos de tú proyecto</div>
+            <div class="hablanos-text">Nos gustaría conocer tu proyecto. Háblanos un poco más para desarrollar un plan de
+            investigación que se ajuste a tus necesidades.</div>
+            <div class="hablanos-button">Hablemos</div>
+        </div>
+    </div>
+    <div class="separador2"></div>
 
 @endsection
