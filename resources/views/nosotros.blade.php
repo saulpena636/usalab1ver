c235265f792f51015adeae85f7dba95819f00035
saulpena636 <suoerpenniya@gmail.com>
2024-09-10 12:15:40 -0600

    nosotros 2ver

--- resources/views/nosotros.blade.php
+++ resources/views/nosotros.blade.php
@@ -78,6 +78,29 @@
     <div class="separador2"></div>
     <div class="colaboracionesContent">
         <div class="colaboracionestitle">Colaboraciones</div>
+        <div class="separador5"></div>
+        <div class="clientesContainer">
+            <div class="clientes-container">
+                <div class="img-wrapper"></div>
+            </div>
+            <div class="colaboracionescont">
+                <div class="colaboracionestxt">
+                    <div class="colaboracionesp"><span>+215</span> Colaboraciones academicas</div>
+                </div>
+
+                <div class="colaboracionestxt">
+                    <div class="colaboracionesp"><span>+35</span> Proyectos comerciales</div>
+                </div>
+
+                <div class="colaboracionestxt">
+                    <div class="colaboracionesp"><span>+25</span> Premios y reconocimientos</div>
+                </div>
+
+                <div class="colaboracionestxt">
+                    <div class="colaboracionesp">Implementación de HCI en Planes de Estudio</div>
+                </div>
+            </div>
+      </div>
     </div>
     <div class="separador2"></div>
     <div class="logrosContent">
